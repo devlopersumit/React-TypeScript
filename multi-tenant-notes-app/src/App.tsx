33d99c117c781ae478205953897e767af9a39809
devlopersumit <sumitsj2002@gmail.com>
2026-07-24 16:22:28 +0530

    feat: update App component structure and add axios API configuration

--- multi-tenant-notes-app/src/App.tsx
+++ multi-tenant-notes-app/src/App.tsx
@@ -1,15 +1,15 @@
 
 
 function App() {
-  
+
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+        Hello world!
+      </h1>
     </>
   )
 }
 
-export default App
+export default App;
--- multi-tenant-notes-app/src/api/axios.ts
+++ multi-tenant-notes-app/src/api/axios.ts
@@ -1,1 +1,8 @@
 import axios from "axios"
+
+const api = axios.create({
+    baseURL:'http://localhost:7000/api/auth',
+    withCredentials:true
+});
+ 
+export default api;
